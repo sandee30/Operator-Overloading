50a5bc89fd4c97faa8b32c792c93dcf6abf865f0
Sandeep.23 <sandeep.of.30@gmail.com>
2025-07-12 22:04:17 +0545

    complex number multiplication

---
+++ p2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+    private:  
+        int real, img;
+    public:
+        void input(){
+            cout<<"Enter real part: ";
+            cin>>real;
+            cout<<"Enter imaginary part: ";
+            cin>>img;
+        }
+
+        Complex operator*(const Complex& c){
+            Complex temp;
+            temp.real = real*c.real - img*c.img;
+            temp.img = img*c.real + real*c.img;
+            return temp;
+        }
+
+        void display(){
+            if(img>=0)
+                cout<<"The product is "<<real<<" + "<<img<<endl;
+            else
+                cout<<"The product is "<<real<<" - "<<-img<<endl;
+        }
+};
+
+int main(){
+    Complex c1,c2,c3;
+    c1.input();
+    c2.input();
+    c3 = c1 * c2;                // c3 = c1.operator*(c2);
+    c3.display();
+    return 0;
+}
